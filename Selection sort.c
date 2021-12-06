80f4954d506dcb9d186390d07cfa4ba56b6c1dc0
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-12-06 19:10:27 +0530

    Update and rename Selection sort to Selection sort.c

---
+++ Selection sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void selectsort(int a[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+                min=j;
+            }
+        }
+        if(min!=i)
+        {
+            int temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+    }
+}
+void print(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int a[100],n;
+    printf("Enter the number of elements\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Before sorting\n");
+    print(a,n);
+    selectsort(a,n);
+    printf("After sorting\n");
+    print(a,n);
+    return 0;
+}
